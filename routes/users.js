ef134c388cfb467432acd71fa6bd57dfeb5102a3
fanyewuji <fanyewuji@163.com>
2022-02-18 08:23:47 -0500

    user registration validator

--- routes/users.js
+++ routes/users.js
@@ -2,12 +2,28 @@ const express = require('express');
 
 const router = express.Router();
 
+const { check, validationResult } = require('express-validator/check');
+
+const User = require('../models/User');
+
 // @route    POST api/users
 // @desc     register a user
 // @acess    Public
 
-router.post('/', (req, res) => {
-    res.send('Register a user');
+router.post('/',
+    [
+        check('name', 'Please enter name')
+        .not()
+        .isEmpty(),
+        check('email', 'Please enter a valid email').isEmail(),
+        check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 }),
+    ],
+    (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status('400').json({ errors: errors.array() });
+        }
+        res.send('request passed');
 });
 
 module.exports = router;

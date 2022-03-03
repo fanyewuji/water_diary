226569847cbd19239f972d7a1b1cc1fef9c79072
fanyewuji <fanyewuji@163.com>
2022-03-03 09:11:26 -0500

    update water record route

--- routes/auth.js
+++ routes/auth.js
@@ -72,7 +72,7 @@ router.post('/',
             );
         } catch (err) {
             console.error(err.message);
-            res.status(500).json({ msg: 'Server Error'})
+            res.status(500).json({ msg: 'Server Error'});
         }
 });
 
--- routes/water.js
+++ routes/water.js
@@ -38,8 +38,40 @@ router.get('/', auth, async (req, res) => {
 // @desc     change drinking water amount for today
 // @acess    Private
 
-router.put('/', (req, res) => {
-    res.send('Change drinking water amount');
+router.put('/:id', 
+    [
+        auth,
+        check('water', 'Water amount must be positive').isFloat({ min: 0 })
+    ], 
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status('400').json({ errors: errors.array() });
+        }
+
+        const { water } = req.body;
+
+        try {
+            let waterRecord = await Water.findById(req.params.id);
+
+            if (!waterRecord) return res.status(404).json({ msg: 'Record not found' });
+
+            // Make sure user owns the record
+            if (waterRecord.user.toString() !== req.user.id) {
+                return res.status(401).json({ msg: 'Not authorized' });
+            }
+
+            waterRecord = await Water.findByIdAndUpdate(req.params.id, 
+                { $set: { water }},
+                { new: true }
+            );
+            
+            res.status(200).json(waterRecord);
+
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).json({ msg: 'Server Error' });
+        }
 });
 
 // @route    GET api/water/all

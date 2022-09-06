5ce3d118fb44249ccb408b6397e8e8b45a5564cb
fanyewuji <fanyewuji@163.com>
2022-09-06 08:36:24 -0400

    add authContext

--- client/src/App.js
+++ client/src/App.js
@@ -4,24 +4,27 @@ import Home from './components/pages/Home';
 import About from './components/pages/About';
 import Navbar from './components/layout/Navbar';
 
+import AuthState from './context/auth/AuthState';
 import WaterState from './context/water/WaterState';
 import './App.css';
 
 const App = () => {
   return (
-    <WaterState>
-      <Router>
-        <>
-        <Navbar title={' Water Diary'}/> 
-        <div className="container">
-          <Routes>
-            <Route path='/' element={<Home />}/>
-            <Route path='/about' element={<About />}/>
-          </Routes>
-        </div>
-        </>
-      </Router>
-    </WaterState>
+    <AuthState>
+      <WaterState>
+        <Router>
+          <>
+          <Navbar title={' Water Diary'}/> 
+          <div className="container">
+            <Routes>
+              <Route path='/' element={<Home />}/>
+              <Route path='/about' element={<About />}/>
+            </Routes>
+          </div>
+          </>
+        </Router>
+      </WaterState>
+    </AuthState>
   );
 }
 
